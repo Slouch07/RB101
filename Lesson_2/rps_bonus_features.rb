a12000f85529da94382b5c8ced3c7f38f2658ed2
Ryan Bowler <rbowler0707@gmail.com>
2020-10-29 09:44:16 -0700

    Refactored code after review recommendations.

--- Lesson_2/rps_bonus_features.rb
+++ Lesson_2/rps_bonus_features.rb
@@ -1,9 +1,17 @@
-VALID_CHOICES = {
+MOVES = {
   'r' => 'rock',
   'p' => 'paper',
   's' => 'scissors',
   'l' => 'lizard',
-  'S' => 'spock'
+  'sp' => 'spock'
+}
+
+WHO_WINS = {
+  'rock' => ['lizard', 'scissors'],
+  'paper' => ['rock', 'spock'],
+  'scissors' => ['paper', 'lizard'],
+  'lizard' => ['spock', 'paper'],
+  'spock' => ['scissors', 'rock']
 }
 
 def prompt(message)
@@ -11,14 +19,10 @@ def prompt(message)
 end
 
 def winner?(player1, player2)
-  (player1 == 'rock' && (player2 == 'scissors' || player2 == 'lizard')) ||
-    (player1 == 'paper' && (player2 == 'rock' || player2 == 'spock')) ||
-    (player1 == 'scissors' && (player2 == 'paper' || player2 == 'lizard')) ||
-    (player1 == 'lizard' && (player2 == 'spock' || player2 == 'paper')) ||
-    (player1 == 'spock' && (player2 == 'rock' || player2 == 'scissors'))
+  WHO_WINS[player1].include?(player2)
 end
 
-def display_results(player, computer)
+def get_result(player, computer)
   if winner?(player, computer)
     'Player wins!'
   elsif winner?(computer, player)
@@ -28,42 +32,49 @@ def display_results(player, computer)
   end
 end
 
+def get_choice(*)
+  Kernel.gets().chomp().downcase()
+end
+
 loop do
+  system "clear"
   prompt("Welcome to RPSLS - First to 5 is the Grand Champion!")
 
   player_score = 0
   computer_score = 0
 
   selection = <<-MSG
-    Please select your weapon:
+    Please select your move:
     'r' - rock
     'p' - paper
     's' - scissors
     'l' - lizard
-    'S' - spock
+    'sp' - spock
   MSG
 
   loop do
     choice = ''
     loop do
       prompt(selection)
-      choice = Kernel.gets().chomp()
+      choice = get_choice(selection)
 
-      if VALID_CHOICES.key?(choice)
+      if MOVES.key?(choice)
         break
       else
         prompt("That's not a valid choice.")
       end
     end
 
-    computer_choice = VALID_CHOICES.values.sample
+    computer_choice = MOVES.values.sample
 
-    prompt("Player chose: #{VALID_CHOICES[choice]}! Computer chose: #{computer_choice}!")
+    prompt("Player: #{MOVES[choice]}! Computer: #{computer_choice}!")
 
-    if display_results(VALID_CHOICES[choice], computer_choice) == "Player wins!"
+    result = get_result(MOVES[choice], computer_choice)
+
+    if result == "Player wins!"
       player_score += 1
       prompt('Player wins!')
-    elsif display_results(VALID_CHOICES[choice], computer_choice) == "Computer wins!"
+    elsif result == "Computer wins!"
       computer_score += 1
       prompt('Computer wins!')
     else
@@ -81,7 +92,7 @@ loop do
     end
   end
 
-  prompt('Would you like to play again? y/n')
+  prompt("Enter 'y' to play again.")
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?('y')
 end
