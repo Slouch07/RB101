c9ac5dcd0f7529cc50d4e50a1cb60a7a43f78c6b
Ryan Bowler <ryanbowler@hotmail.com>
2020-11-16 10:48:08 -0800

    Added exercise files for section.

---
+++ Small_Problems/Easy_4/short_long_short.rb
@@ -0,0 +1,19 @@
+# a method that takes two strings as arguments, determines the longest of the two strings, 
+# and then returns the result of concatenating the shorter string, the longer string, and 
+# the shorter string once again.
+
+def short_long_short(str1, str2)
+  if str1.size < str2.size
+    str1 + str2 + str1
+  else
+    str2 + str1 + str2
+  end
+end
+
+# def short_long_short(str1, str2)
+#   str1.size < str2.size ? str1 + str2 + str1 : str2 + str1 + str2
+# end
+
+p short_long_short('abc', 'defgh') == "abcdefghabc"
+p short_long_short('abcde', 'fgh') == "fghabcdefgh"
+p short_long_short('', 'xyz') == "xyz"
---
+++ Small_Problems/Easy_4/what_century_is_that.rb
@@ -0,0 +1,18 @@
+# A method that takes a year as input and returns the century. 
+# The return value should be a string that begins with the century number, 
+# and ends with st, nd, rd, or th as appropriate for that number.
+
+def century(year)
+  
+
+end
+
+century(2000) == '20th'
+century(2001) == '21st'
+century(1965) == '20th'
+century(256) == '3rd'
+century(5) == '1st'
+century(10103) == '102nd'
+century(1052) == '11th'
+century(1127) == '12th'
+century(11201) == '113th'
